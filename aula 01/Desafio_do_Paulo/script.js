6d116bad95f31feaa6243402fa998d57f04acdbe
Willami Ferreira <willamigabrielf.souza@gmail.com>
2023-02-01 11:02:11 -0300

    arquivos da aula 02 adicionados

--- aula 01/Desafio_do_Paulo/script.js
+++ aula 01/Desafio_do_Paulo/script.js
@@ -1,6 +1,8 @@
 function converter(){
-    var anosLuz = document.querySelector('input[type="number"]');
+    var anosLuz = document.querySelector('input[type="number"]').value;
+    anosLuz = parseInt(anosLuz); //Transformando string em number
     var velocidadeDaLuzEmMetros = 9460528405000020;
-    var anosLuzEmMetros = anosLuz.value * velocidadeDaLuzEmMetros;
-    alert(anosLuz.value + ' anos-luz equivale a ' + anosLuzEmMetros + " de metros.");
+    var anosLuzEmMetros = anosLuz * velocidadeDaLuzEmMetros;
+    
+    alert(anosLuz + ' anos-luz equivale a ' + anosLuzEmMetros + " de metros.");
 }
---
+++ aula 02/desafio_do_Paulo/descrição.md
@@ -0,0 +1,2 @@
+1 - adicionar a quantidade de tentativas feitas
+2 - adicionar o número chutado no alert()
