a1f63ecdf4d2d8759e892cef798f1b1cff72a3bb
Willami Ferreira <willamigabrielf.souza@gmail.com>
2023-02-04 16:40:34 -0300

    Deletando filmes

--- aula 04/script.js
+++ aula 04/script.js
@@ -4,10 +4,12 @@ Guardar todos os filmes adicionados em uma lista/array e percorrer essa lista to
 
 var listaImagens = [];
 var listaTitulos = [];
+var listaTreilers = [];
 var listaFilmes = [];
 
+var containerImagens = document.querySelector("#listaFilmes");
+
 function adicionarFilme() {
-    var containerImagens = document.querySelector("#listaFilmes");
     //limpando o container de imagens para que elas não se repetirem.
     containerImagens.innerHTML = "";
     listaFilmes = []
@@ -15,21 +17,33 @@ function adicionarFilme() {
     //Obtendo e atribuindo à variáveis o link da imagem e título.
     var linkImagem = document.querySelector("#filme").value;
     var titulo = document.querySelector("#titulo").value;
+    var linkTrailer = document.querySelector('#trailer').value;
+
+    //tratamento de erro 
+    var novaFrase = []
+    var palavras = titulo.split(" ");
+
+    for (var i = 0; i < palavras.length; i++) {
+        var novaPalavra = palavras[i][0].toUpperCase() + palavras[i].substr(1).toLowerCase();
+        novaFrase.push(novaPalavra)
+    }
+    novaFrase = novaFrase.join(" ");
 
     //adicionando a nova imagem e titulo em suas respectivas listas.
     listaImagens.push(linkImagem);
-    listaTitulos.push(titulo);
+    listaTreilers.push(linkTrailer);
+    listaTitulos.push(novaFrase);
 
     //adicionando a última imagem da lista dentro do container de imagens
-    for (var i = 0; i < listaImagens.length; i++){
+    for (var i = 0; i < listaImagens.length; i++) {
         var newBannerContainer = document.createElement("div"); //cria um elemento div
 
         newBannerContainer.setAttribute("class", "banner-container"); // adiciona uma class
-        var newBanner = listaImagens[i];
-        newBannerContainer.innerHTML = ` ${listaTitulos[i]} <img src="${newBanner}">`; //usando o innerHTML, é atribuido o nome é imagem ao container criado 
 
-        containerImagens.appendChild(newBannerContainer)//adiciona o novo filme na lista
-        listaFilmes.push(newBannerContainer)//adicionando o novo filme no final da lista de itens.
+        newBannerContainer.innerHTML = ` ${listaTitulos[i]} <a href="${listaTreilers[i]}" target="_blank"><img src="${listaImagens[i]}"></a>`; //usando o innerHTML, é atribuido o nome é imagem ao container criado 
+        
+        listaFilmes.push(newBannerContainer) //adicionando o novo filme no final da lista de itens.
+        containerImagens.appendChild(newBannerContainer) //adiciona o novo filme na lista
     }
 
     //console.log(listaFilmes)
@@ -37,34 +51,42 @@ function adicionarFilme() {
     //limpando os campos
     document.querySelector('#filme').value = "";
     document.querySelector('#titulo').value = "";
+    document.querySelector('#trailer').value = "";
 
 }
 
-function removerFilme(){
+function removerFilme() {
     //Obtendo o nome do filme que deseja excluir
     var filmeParaExcluir = document.querySelector("#filmeaexcluir").value;
+
+    containerImagens.innerHTML = "";
+    //istaFilmes = []
 
     /*Tratamento de erro - deixando a primeira letra da cada parava em caixa alta*/
     var novaFrase = []
     var palavras = filmeParaExcluir.split(" ");
 
-    for (var i = 0; i < palavras.length; i++){
-        var novaPalavra = palavras[i][0].toUpperCase() + palavras[i].substr(1)
+    for (var i = 0; i < palavras.length; i++) {
+        var novaPalavra = palavras[i][0].toUpperCase() + palavras[i].substr(1).toLowerCase();
         novaFrase.push(novaPalavra)
     }
-
     novaFrase = novaFrase.join(" ");
 
-    for (var i = 0; i < listaTitulos.length; i++){
+    //Excluindo elementos
+    for (var i = 0; i < listaImagens.length; i++) {
         if (novaFrase == listaTitulos[i]){
-            //remover o item 
-
-            listaFilmes.splice(i, 1);
-
-        } else {
-            alert("erro")
+            listaImagens.splice(i, 1)
+            listaTitulos.splice(i, 1)
+            listaFilmes.splice(i, 1)
         }
     }
 
+    for(item of listaFilmes){
+        containerImagens.appendChild(item)
+    }
+
+
+
+    //limpando o campo
     document.querySelector('#filmeaexcluir').value = "";
 }
